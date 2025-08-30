95e9ac7be25adb60399780bc7230a7bc08bc436f
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-30 00:18:48 -0400

    Hello level info layer

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
+
+#include <Geode/modify/LevelInfoLayer.hpp>
+class $modify(LevelInfoLayer) {
+	bool init(GJGameLevel* level, bool challenge) {
+		if (!LevelInfoLayer::init(level, challenge)) return false;
+
+		log::info("Hello level info layer");
+
+		return true;
+	}
+};
