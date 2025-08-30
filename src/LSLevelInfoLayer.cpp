c587c4caeb38e965ab3f9618b42df65bdb4f679b
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-30 01:04:30 -0400

    Add set shortcut button to online level layer

--- src/LSLevelInfoLayer.cpp
+++ src/LSLevelInfoLayer.cpp
@@ -3,12 +3,22 @@
 using namespace geode::prelude;
 
 #include <Geode/modify/LevelInfoLayer.hpp>
-class $modify(LevelInfoLayer) {
+class $modify(LSLevelInfoLayer, LevelInfoLayer) {
 	bool init(GJGameLevel* level, bool challenge) {
 		if (!LevelInfoLayer::init(level, challenge)) return false;
 
-		log::info("Hello level info layer");
+		if (CCMenu* leftSideMenu = geode::cast::typeinfo_cast<CCMenu*>(getChildByID("left-side-menu"))) {
+			CircleButtonSprite* setShortcutSprite = CircleButtonSprite::createWithSprite("logo.png"_spr);
+			CCMenuItemSpriteExtra* setShortcutButton = CCMenuItemSpriteExtra::create(setShortcutSprite, this, menu_selector(LSLevelInfoLayer::setShortcut));
+			setShortcutButton->setID("set-shortcut-button"_spr);
+			leftSideMenu->addChild(setShortcutButton);
+			leftSideMenu->updateLayout();
+		}
 
 		return true;
+	}
+
+	void setShortcut(CCObject* sender) {
+		log::info("setShortcut");
 	}
 };
