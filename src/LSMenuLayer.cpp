6b670fa080ba260f5325bb1ce38bbbb1e78b62e4
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-08-31 00:44:18 -0400

    Move openShortcut to LevelShortcuts

--- src/LSMenuLayer.cpp
+++ src/LSMenuLayer.cpp
@@ -4,8 +4,6 @@ using namespace geode::prelude;
 
 #include "LevelShortcuts.hpp"
 
-void showError(std::string error);
-
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(LSMenuLayer, MenuLayer) {
     $override
@@ -15,7 +13,7 @@ class $modify(LSMenuLayer, MenuLayer) {
         if (Mod::get()->getSettingValue<bool>("enabled")) {
             if (CCMenu* rightSideMenu = geode::cast::typeinfo_cast<CCMenu*>(getChildByID("right-side-menu"))) {
                 CCSprite* openShortcutSprite = CCSprite::create("openShortcut.png"_spr);
-                CCMenuItemSpriteExtra* openShortcutButton = CCMenuItemSpriteExtra::create(openShortcutSprite, this, menu_selector(LSMenuLayer::openShortcut));
+                CCMenuItemSpriteExtra* openShortcutButton = CCMenuItemSpriteExtra::create(openShortcutSprite, this, menu_selector(LSMenuLayer::onOpenShortcut));
                 openShortcutButton->setID("open-shortcut-button"_spr);
                 rightSideMenu->addChild(openShortcutButton);
                 rightSideMenu->updateLayout();
@@ -25,51 +23,8 @@ class $modify(LSMenuLayer, MenuLayer) {
         return true;
     }
     
-    void openShortcut(CCObject* sender) {
-        int shortcutType = Mod::get()->getSavedValue("shortcut-type", 0);
-        if (!shortcutType) {
-            showError("No level shortcut found! Set one by entering a level screen and pressing the <cg>Set Shortcut</c> button.");
-            return;
-        }
-        GJGameLevel* level;
-        switch (shortcutType) {
-            case ONLINE: {
-                int levelID = Mod::get()->getSavedValue(DATA_FOR_TYPE(ONLINE), 0);
-                if (!levelID) {
-                    showError("No level ID was found! What.");
-                    return;
-                }
-                GJGameLevel* level = GameLevelManager::get()->getSavedLevel(levelID);
-                if (!level) {
-                    showError(fmt::format("No online level with ID {} was found! Perhaps the level was deleted?", levelID));
-                    return;
-                }
-                switchToScene(LevelInfoLayer::create(level, false));
-                break;
-            }
-            case EDITOR: {
-                int levelIndex = Mod::get()->getSavedValue(DATA_FOR_TYPE(EDITOR), -1);
-                if (levelIndex == -1) {
-                    showError("No level index was found! What.");
-                    return;
-                } 
-                GJGameLevel* level = geode::cast::typeinfo_cast<GJGameLevel*>(LocalLevelManager::get()->m_localLevels->objectAtIndex(levelIndex));
-                if (!level) {
-                    showError(fmt::format("No local level with index {} was found! Perhaps the level was deleted?", levelIndex));
-                    return;
-                }
-                switchToScene(EditLevelLayer::create(level));
-                break;
-            }
-        }
-        
+    void onOpenShortcut(CCObject* sender) {
+        openShortcut();
     }
 };
 
-void showError(std::string error) {
-    FLAlertLayer::create(
-        "No Shortcut Found",
-        error,
-        "OK"
-    )->show();
-}
--- src/LevelShortcuts.hpp
+++ src/LevelShortcuts.hpp
@@ -11,6 +11,8 @@ using namespace geode::prelude;
 #define STRING(x) STRINGIFY(x)
 #define DATA_FOR_TYPE(type) "data-for-type-" STRING(type)
 
+void showError(std::string error);
+
 inline void setShortcut(int type, auto data) {
     Mod::get()->setSavedValue("shortcut-type", type);
     Mod::get()->setSavedValue(fmt::format("data-for-type-{}", type), data);
@@ -20,3 +22,50 @@ inline void setShortcut(int type, auto data) {
         "OK"
     )->show();
 };
+
+inline void openShortcut() {
+    int shortcutType = Mod::get()->getSavedValue("shortcut-type", 0);
+        if (!shortcutType) {
+            showError("No level shortcut found! Set one by entering a level screen and pressing the <cg>Set Shortcut</c> button.");
+            return;
+        }
+        GJGameLevel* level;
+        switch (shortcutType) {
+            case ONLINE: {
+                int levelID = Mod::get()->getSavedValue(DATA_FOR_TYPE(ONLINE), 0);
+                if (!levelID) {
+                    showError("No level ID was found! What.");
+                    return;
+                }
+                GJGameLevel* level = GameLevelManager::get()->getSavedLevel(levelID);
+                if (!level) {
+                    showError(fmt::format("No online level with ID {} was found! Perhaps the level was deleted?", levelID));
+                    return;
+                }
+                switchToScene(LevelInfoLayer::create(level, false));
+                break;
+            }
+            case EDITOR: {
+                int levelIndex = Mod::get()->getSavedValue(DATA_FOR_TYPE(EDITOR), -1);
+                if (levelIndex == -1) {
+                    showError("No level index was found! What.");
+                    return;
+                } 
+                GJGameLevel* level = geode::cast::typeinfo_cast<GJGameLevel*>(LocalLevelManager::get()->m_localLevels->objectAtIndex(levelIndex));
+                if (!level) {
+                    showError(fmt::format("No local level with index {} was found! Perhaps the level was deleted?", levelIndex));
+                    return;
+                }
+                switchToScene(EditLevelLayer::create(level));
+                break;
+            }
+        }
+}
+
+inline void showError(std::string error) {
+    FLAlertLayer::create(
+        "No Shortcut Found",
+        error,
+        "OK"
+    )->show();
+}
